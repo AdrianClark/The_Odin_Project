98955fb1d1620c283f8f8c18de269745b70dc552
Adrian Clark <adrian@adrianclark.me>
2015-06-14 23:11:44 -0400

    added base files for the Javascript/jQuery 'etch-a-sketch' grid project.

---
+++ JS:jQuery WD101 Sketch Grid/jsjqgrid.js
@@ -0,0 +1,3 @@
+//http://www.theodinproject.com/web-development-101/javascript-and-jquery
+//The details to this project can be found at the link above. There are a lot of good
+//student solutions that I hope I can compare my work with. 
